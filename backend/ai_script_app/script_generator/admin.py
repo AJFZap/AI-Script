2b421b382fca8923a62118781b711eaa50e942f7
AJFZap <ariel_zappia@hotmail.com>
2024-05-14 11:48:14 -0300

    Improved file hierarchy

--- backend/ai_script_app/script_generator/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import ScriptsList, CustomUser
-
-# Register your models here.To be able to see them as an Admin.
-
-admin.site.register(ScriptsList)
-admin.site.register(CustomUser)
--- backend/ai_script_app/script_generator/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ScriptGeneratorConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'script_generator'
