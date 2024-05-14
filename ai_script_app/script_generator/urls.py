d719e77484a81d67cdb253ab91e028e9ac8084f6
AJFZap <ariel_zappia@hotmail.com>
2024-05-14 14:37:32 -0300

    Fixed error with reset_password/, script_details/  page

--- ai_script_app/script_generator/urls.py
+++ ai_script_app/script_generator/urls.py
@@ -19,8 +19,8 @@ urlpatterns = [
     path('delete/<int:pk>/', views.delete_script, name='delete_script'),
     path('verify_account/<uidb64>/<token>', views.activate, name='verify_account'),
     
-    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="password_reset.html"), name='reset_password'),
+    path('reset_password//', auth_views.PasswordResetView.as_view(template_name="password_reset.html"), name='reset_password'),
     path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name="password_reset_done.html"), name='password_reset_done'),
-    path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="password_reset_confirm.html"), name='password_reset_confirm'),
     path('reset_password_success/', auth_views.PasswordResetCompleteView.as_view(template_name="password_reset_complete.html"), name='password_reset_complete'),
 ]
